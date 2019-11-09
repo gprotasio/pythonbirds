a9ddafa8b07efff85d9f1e39b50989775e57a89e
gprotasio <georgeprotasio@gmail.com>
2019-11-09 13:03:11 -0300

    Criados atributos das instâncias name e age

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome=None, age=40):
+    def __init__(self, name=None, age=None, children=None):
         self.age = age
-        self.nome = nome
+        self.name = name
+        self.children = children
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-    print(p.nome)
+    print(p.name)
     p.nome = 'George'
-    print(p.nome)
+    print(p.name)
     print(p.age)
