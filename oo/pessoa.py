c4392bad80d2afae7247514dae0492e4e4114c15
gprotasio <georgeprotasio@gmail.com>
2019-11-09 15:02:32 -0300

    Criado atribuito complexo children

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(carlos))
     print(id(carlos))
     print(carlos.cumprimentar())
+    print(carlos.name)
     print(carlos.age)
     for children in carlos.children:
         print(children.name)
