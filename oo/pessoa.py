132b0afb42f96dbb85356e17f4d7df1e8116463f
gprotasio <georgeprotasio@gmail.com>
2019-11-09 13:14:30 -0300

    Criado atribuito complexo Children

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,19 +2,19 @@ class Pessoa:
     def __init__(self, name=None, age=None, children=None):
         self.age = age
         self.name = name
-        self.children = children
+        self.children = list(children)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    i = Pessoa(40)
+    George = Pessoa(name='George')
+    Carlos = Pessoa(George, name='Carlos')
     print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.name)
-    p.name = 'George'
-    print(p.name)
-    print(p.age)
+    print(id(Carlos))
+    print(Carlos.cumprimentar())
+    print(Carlos.name)
+    print(Carlos.age)
+    for children in Carlos.children:
+        print(children.name)
